8e8698c2774567b5fd8aca0f7459cdae7e77c98a
amin <amin.karimi@bluebitsoft.com>
2020-03-23 20:02:21 +0430

    add banking data

---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext _context;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IEnumerable<Account> GetAccount()
+        {
+            return _context.Accounts;
+        }
+    }
+}
