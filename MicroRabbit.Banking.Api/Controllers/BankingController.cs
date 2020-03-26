ca212572a66beaeccc9b47a44a57a6a3ace7c89d
amin <amin.karimi1992@gmail.com>
2020-03-26 19:46:03 +0430

    add rebbitmq to publish data

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,5 +26,14 @@ namespace MicroRabbit.Banking.Api.Controllers
         {
             return Ok(_accountService.GetAccounts());
         }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            
+            _accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
+        }
+        
     }
 }
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,11 @@
+using MediatR;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -14,7 +18,10 @@ namespace MicroRabbit.Infra.IoC
         public static void RegisterServices(IServiceCollection services)
         {
             // Domain Bus 
-            services.AddTransient<IEventBus, RabbitMQBus>(); 
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            
+            // Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
             
             // Application Services
             services.AddTransient<IAccountService, AccountService>();
