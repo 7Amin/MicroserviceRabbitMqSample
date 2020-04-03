c6efb5b45013f90d340f64e1be30d0090f9552e6
amin <amin.karimi1992@gmail.com>
2020-04-03 14:38:44 +0430

    add transfer logs

--- MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit.Transfer.Api/Startup.cs
@@ -2,20 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroRabbit.Transfer.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TransferDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+            });
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transfer Microservice", Version = "v1"});
+            });
+
+            services.AddMediatR(typeof(Startup));
+            
+            RegisterServices(services);
+        }
+        
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +58,20 @@ namespace MicroRabbit.Transfer.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservice V1");
             });
+            app.UseMvc();
+            
         }
     }
 }
---
+++ MicroRabbit.Transfer.Data/Context/TransferDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Data.Context
+{
+    public class TransferDbContext : DbContext
+    {
+        public TransferDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TransferLog> Accounts { get; set; }
+    }
+}
